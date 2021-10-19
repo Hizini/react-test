8fa00149f7d126cc532f8f7b2f656a4ae61d9c02
KimHeeJin <ejfdnrl@naver.com>
2021-10-19 14:57:53 +0900

    class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+//class형 컴포넌트 생성
+class App extends Component{
+  render() {
+    const name = '희진' 
+    return <div>안뇽, <h1>나는 {name}</h1></div> 
+  }
 }
-
 export default App;
