1ca75aca363b5d02f4e53a54cc2e1afb7c90505e
KimHeeJin <ejfdnrl@naver.com>
2021-10-19 15:03:30 +0900

    class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'; 
 
 //class형 컴포넌트 생성
 class App extends Component{
   render() {
-    const name = '희진' 
-    return <div>안뇽, <h1>나는 {name}</h1></div> 
+    const name = '희진'
+    return <div>안뇽, <h1>나는 {name}</h1></div>
   }
 }
 export default App;
